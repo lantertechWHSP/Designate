67dc0f6c9c2420644bef66cff721ab6ac2a4051f
Carl Benedict Rain <carl.rain@rainfoundry.com>
2023-08-07 13:56:39 +1000

    master * Implement feeding data

--- components/layouts/layout.tsx
+++ components/layouts/layout.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
+export interface ILayout {
+    slug?:string;
+    title?:string;
+    preview?:boolean;
+}
+
 export default function Layout({ layout, children }:any) : JSX.Element {
     return (
         <>
             <header>
                 Header
             </header>
+            {
+                layout.title && <h1>
+                    {
+                        layout.title
+                    }
+              </h1>
+            }
             {children}
             <footer>
                 Footer
