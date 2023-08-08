30246f5ad66d04651c4b3d68e6bf3ccd80a19803
Carl Benedict Rain <carl.rain@rainfoundry.com>
2023-08-08 12:02:11 +1000

    master * Tinker

---
+++ lib/theme/theme.tsx
@@ -0,0 +1,10 @@
+import { extendBaseTheme } from '@chakra-ui/react';
+
+// import chakraTheme from '@chakra-ui/theme';
+
+export const theme = extendBaseTheme({
+    fonts: {
+        heading: `'Arimado', sans-serif`,
+        body: `'Arimado', sans-serif`,
+    },
+});
