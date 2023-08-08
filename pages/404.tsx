5813bf8ede59b0b8fbb421ed20547505796a351c
Carl Benedict Rain <carl.rain@rainfoundry.com>
2023-08-08 16:49:14 +1000

    master * Update

---
+++ pages/404.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import type { NextPage } from 'next';
+import Layout from "~/components/layouts/Layout";
+import { doQuery, queries } from "~/dato/api";
+import { getLayoutData } from '~/lib/utils';
+
+export async function getStaticProps({ _params, preview }) : Promise<any> {
+    const site = await doQuery(queries.site);
+    const page = {
+        title: 'Error (404)',
+        slug: '404'
+    };
+    const layout = getLayoutData(site, page, preview);
+
+    return { props: { layout } };
+}
+
+const ErrorPage : NextPage = ({layout}:any) : JSX.Element => {
+    return <Layout layout={layout}>
+        <p>
+            Page Not Found
+        </p>
+    </Layout>;
+};
+
+export default ErrorPage;
