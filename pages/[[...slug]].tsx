e0fbd6a5ed65fd3bc77692d28fd3629620a78128
Carl Benedict Rain <carl.rain@rainfoundry.com>
2023-08-08 12:09:53 +1000

    master * Add chakra library * Add fonts * Sort out templating for react

---
+++ pages/[[...slug]].tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import type { NextPage } from 'next';
+import Default from '~/components/layouts/Default';
+import { ModularContent } from '~/components/ModularContent';
+import { doQuery, queries } from '~/dato/api';
+import { getLayoutData, getBlocks } from '~/lib/utils';
+
+export async function getStaticPaths() : Promise<any> {
+    const pages = await doQuery(queries.pages).then(({ pages }) => pages);
+
+    const paths = pages
+        .map((page) => {
+            const slug = page.slug === 'home' ? [''] : page.slug.split('/');
+            return { params: { slug } };
+        });
+
+    return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params, preview }) : Promise<any> {
+    const { slug: slugRaw } = params;
+
+    const slug = slugRaw ? slugRaw.join('/') : 'home';
+    const site = await doQuery(queries.site);
+    const page = await doQuery(queries.page, { slug }, preview).then(
+        ({ page }) => page
+    );
+
+    const layout = getLayoutData(site, page, preview);
+    const blocks = await getBlocks(page);
+
+    return { props: { layout, blocks } };
+}
+
+const Page : NextPage = ({layout, blocks}:any)  : JSX.Element => {
+    return (
+        <Default layout={layout}>
+            <ModularContent content={blocks} />
+        </Default>
+    );
+};
+
+export default Page;
