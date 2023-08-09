e07163940bbe8ea2b6c976e08906e5eef5219695
Carl Benedict Rain <carl.rain@rainfoundry.com>
2023-08-09 12:12:55 +1000

    master * Tinker

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,8 +1,9 @@
 import { ReactNode, useState } from 'react';
-import { Container, Flex, Box, Popover, Link, PopoverTrigger, PopoverContent, useDisclosure } from '@chakra-ui/react';
+import { Container, Flex, Box, Popover, Link, Button, PopoverTrigger, PopoverContent, useDisclosure } from '@chakra-ui/react';
 import { DatoLink } from '~/components/elements/datoLink';
 import { motion } from 'framer-motion';
 import useDocumentScroll from 'hooks/useDocumentScroll';
+import {Icon, Icons} from '~/components/elements/icon';
 
 interface IMenuItem {
     link: {
@@ -15,7 +16,7 @@ interface IMenuItem {
 const MotionBox = motion(Box);
 
 const Header = ({ menu }) : ReactNode => {
-    const { isOpen } = useDisclosure();
+    const { isOpen, onToggle } = useDisclosure();
 
     const [isScrolledDown, setIsScrolledDown] = useState(false);
     const [isMinimumScrolled, setIsMinimumScrolled] = useState(false);
@@ -32,49 +33,66 @@ const Header = ({ menu }) : ReactNode => {
                 animate={{
                     opacity: !isOpen && isScrolledDown && isMinimumScrolled ? 0 : 1,
                 }}>
-                <DesktopNav menu={menu} />
+                <Box py={4} style={{
+                    borderBottom: '1px solid #ccc',
+                    background: '#f0f0f0',
+                }} h="60px">
+                    <Container>
+                        <Flex>
+                            <Box width={200}>
+                                <Link href="/">Home</Link>
+                            </Box>
+                            <DesktopNav menu={menu} />
+                            <Flex display={['flex', , ,'none']} flex={1}>
+                                <Box flex={1} />
+                                <Button onClick={onToggle}>
+                                    {
+                                        isOpen ? <Icon icon={Icons.Hamburger} w={20} h={20} /> : <Icon icon={Icons.Cross} w={20} h={20} />
+                                    }
+                                </Button>
+                            </Flex>
+                        </Flex>
+                    </Container>
+                </Box>
             </MotionBox>
+            <MobileNav menu={menu} isOpen={isOpen} />
         </Box>
     </Box>;
 };
 
 const DesktopNav = ({menu}) : ReactNode => {
-    return <Box py={4} style={{
-        borderBottom: '1px solid #ccc',
-        background: '#f0f0f0',
-    }} h="60px">
-        <Container>
-            <Flex>
-                <Box width={200}>
-                    <Link href="/">Home</Link>
-                </Box>
-                <Box as="nav">
-                    {
-                        Array.isArray(menu) && menu.length > 0 && menu.map((item:IMenuItem, index:number) => {
-                            return Array.isArray(item.children) && item.children.length > 0 ? <Popover trigger="hover" placement="bottom-start" key={index}>
-                                <PopoverTrigger>
-                                    <DatoLink {...item} px={2} />
-                                </PopoverTrigger>
-                                <PopoverContent>
-                                    {
-                                        item.children && <Box style={{ background: '#fff', border: '1px solid #ccc' }} py={2} px={4}>
-                                            {
-                                                item.children.map((child:IMenuItem, childIndex:number) => {
-                                                    return <Box py={2} key={childIndex}>
-                                                        <DatoLink {...child} />
-                                                    </Box>;
-                                                })
-                                            }
-                                        </Box>
-                                    }
-                                </PopoverContent>
-                            </Popover> : <DatoLink {...item} px={2} key={index} />;
-                        })
-                    }
-                </Box>
-            </Flex>
-        </Container>
+    return <Box as="nav" display={['none', , ,'block']}>
+        {
+            Array.isArray(menu) && menu.length > 0 && menu.map((item:IMenuItem, index:number) => {
+                return Array.isArray(item.children) && item.children.length > 0 ? <Popover trigger="hover" placement="bottom-start" key={index}>
+                    <PopoverTrigger>
+                        <DatoLink {...item} px={2} />
+                    </PopoverTrigger>
+                    <PopoverContent>
+                        {
+                            item.children && <Box style={{ background: '#fff', border: '1px solid #ccc' }} py={2} px={4}>
+                                {
+                                    item.children.map((child:IMenuItem, childIndex:number) => {
+                                        return <Box py={2} key={childIndex}>
+                                            <DatoLink {...child} />
+                                        </Box>;
+                                    })
+                                }
+                          </Box>
+                        }
+                    </PopoverContent>
+                </Popover> : <DatoLink {...item} px={2} key={index} />;
+            })
+        }
     </Box>;
 };
+
+const MobileNav = ({_menu, isOpen}) : ReactNode => {
+    return <Box as="nav" display={['block', , ,'none']}>
+        {
+            isOpen ? <div>Carl</div> : <div>Reign</div>
+        }
+    </Box>
+}
 
 export default Header;
