72268608118a92a76dab7527292c67dd3a884b5a
Carl Benedict Rain <carl.rain@rainfoundry.com>
2023-08-09 13:43:13 +1000

    master * Update code

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,9 +1,14 @@
-import { ReactNode, useState } from 'react';
-import { Container, Flex, Box, Popover, Link, Button, PopoverTrigger, PopoverContent, useDisclosure } from '@chakra-ui/react';
+import { ReactNode, useState, useEffect, useRef } from 'react';
+import { Container, Flex, Box, Popover, Link, Button, PopoverTrigger, PopoverContent, useDisclosure, Collapse } from '@chakra-ui/react';
 import { DatoLink } from '~/components/elements/datoLink';
 import { motion } from 'framer-motion';
 import useDocumentScroll from 'hooks/useDocumentScroll';
-import {Icon, Icons} from '~/components/elements/icon';
+import {
+    enableBodyScroll,
+    disableBodyScroll,
+    clearAllBodyScrollLocks
+} from 'body-scroll-lock';
+import { Icon, Icons } from '~/components/elements/icon';
 
 interface IMenuItem {
     link: {
@@ -47,7 +52,7 @@ const Header = ({ menu }) : ReactNode => {
                                 <Box flex={1} />
                                 <Button onClick={onToggle}>
                                     {
-                                        isOpen ? <Icon icon={Icons.Hamburger} w={20} h={20} /> : <Icon icon={Icons.Cross} w={20} h={20} />
+                                        isOpen ? <Icon icon={Icons.Cross} w={20} h={20} /> : <Icon icon={Icons.Hamburger} w={20} h={20} />
                                     }
                                 </Button>
                             </Flex>
@@ -78,7 +83,7 @@ const DesktopNav = ({menu}) : ReactNode => {
                                         </Box>;
                                     })
                                 }
-                          </Box>
+                            </Box>
                         }
                     </PopoverContent>
                 </Popover> : <DatoLink {...item} px={2} key={index} />;
@@ -87,11 +92,84 @@ const DesktopNav = ({menu}) : ReactNode => {
     </Box>;
 };
 
-const MobileNav = ({_menu, isOpen}) : ReactNode => {
-    return <Box as="nav" display={['block', , ,'none']}>
-        {
-            isOpen ? <div>Carl</div> : <div>Reign</div>
+const MobileNav = ({menu, isOpen}) : ReactNode => {
+    const scrollRef = useRef();
+
+    useEffect(() => {
+        if (scrollRef.current) {
+            if (isOpen) {
+                disableBodyScroll(scrollRef.current);
+            } else {
+                enableBodyScroll(scrollRef.current);
+            }
         }
+        return () => {
+            clearAllBodyScrollLocks();
+        };
+    }, [scrollRef, isOpen]);
+
+    return <MotionBox as="nav"
+                      position="sticky"
+                      left={0}
+                      right={0}
+                      h="calc(100vh - 34px)"
+                      zIndex={isOpen ? 100 : 0}
+                      display={['block', , ,'none']}
+                      pointerEvents={ !isOpen ? 'none' : 'all'}
+                      backgroundColor={'#f0f0f0'}
+                      opacity={0}
+                      animate={{
+                          opacity: !isOpen ? 0 : 1
+                      }}>
+        <Box ref={scrollRef} overflowY="auto" h="100%">
+            <Box>
+                {
+                    Array.isArray(menu) && menu.length > 0 && menu.map((item:IMenuItem, index:number) => {
+                        return <MobileNavItem item={item} key={index} />;
+                    })
+                }
+            </Box>
+        </Box>
+    </MotionBox>
+}
+
+const MobileNavItem = ({item}) : ReactNode => {
+    const { isOpen, onToggle } = useDisclosure();
+    const hasChildren = Array.isArray(item.children) && item.children.length > 0;
+    const handleClick = (hasChildren) => {
+        if (hasChildren) {
+            onToggle();
+        }
+        return;
+    };
+
+    return <Box>
+        <Flex px={4} py={3} style={{
+            borderBottom: '1px solid #ccc',
+        }}>
+            <DatoLink {...item} />
+            <Box flex={1} />
+            {
+                hasChildren && <Button onClick={handleClick}>
+                    <Box
+                      width="16px"
+                      height="16px"
+                      transition={'all 300ms ease'}
+                      transform={isOpen ? 'rotate(180deg)' : ''}>
+                      <Icon icon={Icons.ChevronDown} w={16} h={16}/>
+                    </Box>
+                </Button>
+            }
+        </Flex>
+        <Collapse in={isOpen} animateOpacity>
+            {
+                item.children.map((child:IMenuItem, childIndex:number) => {
+                    return <Box px={4} py={2} key={childIndex}>
+                        <DatoLink {...child} />
+                    </Box>;
+                })
+            }
+        </Collapse>
     </Box>
 }
 
