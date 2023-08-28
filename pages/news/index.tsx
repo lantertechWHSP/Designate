3c4c86c5dd576a68660599faa2ff5d27b0065c12
Carl Benedict Rain <carl.rain@rainfoundry.com>
2023-08-28 10:13:24 +1000

    master * Add pagination

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type { NextPage } from 'next';
 import Layout from '~/components/layouts/Layout';
 import { ModularContent } from '~/components/ModularContent';
 import { doQuery, queries } from '~/dato/api';
 import { getLayoutData, getBlocks } from '~/lib/utils';
 import { LatestPostCard } from '~/components/elements/cards/latestPostCard';
-import { Container, SimpleGrid } from '@chakra-ui/react';
-import { Heading } from '@chakra-ui/react';
+import { Heading, Button, Text, Container, SimpleGrid, Box, Spinner } from '@chakra-ui/react';
 
 export async function getStaticProps({ preview }) : Promise<any> {
     const slug = 'news';
@@ -15,15 +14,92 @@ export async function getStaticProps({ preview }) : Promise<any> {
         ({ page }) => page
     );
 
-    const latestPosts = await doQuery(queries.latestPosts, { first: 3 }).then(({ posts }) => posts || []);
+    const latestPosts = await doQuery(queries.latestPosts, { first: 1 }).then(({ posts }) => posts || []);
+    const allPostsMeta = await doQuery(queries.postsMeta).then(({ postsMeta }) => postsMeta || {});
 
     const layout = getLayoutData(site, page, preview);
     const blocks = await getBlocks(page);
 
-    return { props: { layout, blocks, latestPosts } };
+    return { props: { layout, blocks, latestPosts, allPostsMeta } };
 }
 
-const NewsPage : NextPage = ({layout, blocks, latestPosts}:any) : JSX.Element => {
+const LatestPosts = ({ latestPosts, postsMeta }) => {
+    const [page, setPage] = useState(1);
+    const [posts, setPosts] = useState(latestPosts); // Iniitial posts
+    const [isLoading, setIsLoading] = useState(false);
+    const [noMorePosts, setNoMorePosts] = useState(false);
+    const [couldNotLoadPosts, setCouldNotLoadPosts] = useState(false);
+
+    const [totalPosts, _setTotalPosts] = useState(postsMeta.count || posts.length);
+    const itemsPerPage = 1;
+
+    const loadMorePosts = () => {
+        setIsLoading(true);
+
+        doQuery(queries.latestPosts, { isFeatured: true, first: itemsPerPage, skip: page * itemsPerPage }).then(({ posts }) => posts || []).then((newPosts) => {
+            console.log(newPosts);
+            if(newPosts.length > 0) {
+                setPosts([...posts, ...newPosts]);
+                setPage(page + 1);
+            }
+            else {
+                setTimeout(() => {
+                    setNoMorePosts(true);
+                }, 250);
+
+                setTimeout(() => {
+                    setNoMorePosts(false)
+                }, 5000);
+            }
+        }).catch(() => {
+            setCouldNotLoadPosts(true);
+            setTimeout(() => {
+                setCouldNotLoadPosts(false)
+            }, 5000);
+        }).finally(() => {
+            setTimeout(() => {
+                setIsLoading(false);
+            }, 250);
+        })
+    }
+
+    return <>
+        {
+            (Array.isArray(posts) && posts.length > 0) ? <><SimpleGrid columns={[1, 2, 3]} spacing={[0, 8]}>
+                {
+                    posts.map((post, index) => {
+                        return <LatestPostCard {...post} key={index}/>;
+                    })
+                }
+            </SimpleGrid>
+                {
+                    (noMorePosts || posts.length === totalPosts) && <Box>
+                        <Text variant="caption" color="grey">No more Posts to load</Text>
+                    </Box>
+                }
+                {
+                    couldNotLoadPosts && <Box>
+                        <Text variant="caption" color="grey">Could not load Posts</Text>
+                    </Box>
+                }
+                {
+                    posts.length < totalPosts && <Button color="sunlight" onClick={loadMorePosts} px={0} rightIcon={isLoading && <Spinner size='sm' />}>
+                        <Text textDecoration="underline" d="inline-block">
+                            Load Moar!
+                        </Text>
+                    </Button>
+                }
+
+            </> : <>
+                <Box>
+                    <Text variant="caption" color="grey">No Posts</Text>
+                </Box>
+            </>
+        }
+    </>
+}
+
+const NewsPage : NextPage = ({layout, blocks, latestPosts, allPostsMeta}:any) : JSX.Element => {
     return (
         <Layout layout={layout}>
             <Container>
@@ -31,15 +107,7 @@ const NewsPage : NextPage = ({layout, blocks, latestPosts}:any) : JSX.Element =>
             </Container>
             <ModularContent content={blocks} />
             <Container>
-                {
-                    (Array.isArray(latestPosts) && latestPosts.length > 0) && <SimpleGrid columns={[1, 2, 3]} spacing={[0, 8]}>
-                        {
-                            latestPosts.map((post, index) => {
-                                return <LatestPostCard {...post} key={index}/>;
-                            })
-                        }
-                    </SimpleGrid>
-                }
+                <LatestPosts latestPosts={latestPosts} postsMeta={allPostsMeta} />
             </Container>
         </Layout>
     );
