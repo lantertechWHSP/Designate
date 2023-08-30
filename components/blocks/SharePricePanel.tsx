3bd8d815556750371b815335af5b868998ace286
Carl Benedict Rain <carl.rain@rainfoundry.com>
2023-08-30 15:55:11 +1000

    master * Update

--- components/blocks/SharePricePanel.tsx
+++ components/blocks/SharePricePanel.tsx
@@ -22,44 +22,44 @@ const SharePricePanel = ({ _title }:any) : ReactNode => {
 
     let selectedSymbolLabel = '';
 
-    const scrollToHistoricPriceTable = () => {
+    const scrollToHistoricPriceTable = () : void => {
         const scrollDiv = document.getElementById("historicalPrice").offsetTop;
         window.scrollTo({ top: scrollDiv - 120, behavior: 'smooth'});
-    }
+    };
 
     return <ContentBlock contain={false}>
         <Box as={YourIR}>
             <Box py={12} background="lightGrey3">
                 <Container>
                     <Grid templateColumns='repeat(5, 1fr)'
-                          gap={8}>
+                        gap={8}>
                         <GridItem colSpan={2}>
                             <Box borderTop="1px solid"
-                                 borderBottom="1px solid"
-                                 borderColor="grey"
-                                 py={4}
-                                 mb={8}>
+                                borderBottom="1px solid"
+                                borderColor="grey"
+                                py={4}
+                                mb={8}>
                                 <Text fontSize={['24px']}
-                                      lineHeight={['26px']}
-                                      mb={4}>
+                                    lineHeight={['26px']}
+                                    mb={4}>
                                     <Text as="span" fontWeight={500} data-yourir="shortName"></Text>{'\u00A0'}
                                     <Text as="span" color="steelBlue3"><span data-yourir="market"></span>:<span data-yourir="symbol"></span></Text>
                                 </Text>
                                 <Text fontSize={['96px']}
-                                      lineHeight={['106px']}
-                                      fontWeight={500}
-                                      data-yourir="price showCurrency=true minDecimals=2 maxDecimals=2" />
+                                    lineHeight={['106px']}
+                                    fontWeight={500}
+                                    data-yourir="price showCurrency=true minDecimals=2 maxDecimals=2" />
                                 <Box fontSize={['16px']}
-                                     lineHeight={['30px']}
-                                     fontWeight={500}
-                                     sx={{
-                                         '.yourir-positive': {
-                                             color: 'green'
-                                         },
-                                         'yourir-negative': {
-                                             color: 'red'
-                                         }
-                                     }}>
+                                    lineHeight={['30px']}
+                                    fontWeight={500}
+                                    sx={{
+                                        '.yourir-positive': {
+                                            color: 'green'
+                                        },
+                                        'yourir-negative': {
+                                            color: 'red'
+                                        }
+                                    }}>
                                     <Flex align="baseline" data-yourir="changeSignCSS">
                                         <Box
                                             data-yourir="changeSignCSS"
@@ -85,9 +85,9 @@ const SharePricePanel = ({ _title }:any) : ReactNode => {
                             <Box>
                                 <label>Market Cap</label>
                                 <Text fontSize={['20px']}
-                                      lineHeight={['26px']}
-                                      fontWeight={500}
-                                      data-yourir="marketCap showCurrency=true minDecimals=2 maxDecimals=2"></Text>
+                                    lineHeight={['26px']}
+                                    fontWeight={500}
+                                    data-yourir="marketCap showCurrency=true minDecimals=2 maxDecimals=2"></Text>
                             </Box>
                         </GridItem>
                         <GridItem colSpan={3}>
@@ -101,49 +101,49 @@ const SharePricePanel = ({ _title }:any) : ReactNode => {
                                 </defs>
                             </svg>
                             <Box w="100%"
-                                 sx={{
-                                     '.yourir-chart': {
-                                         padding: '40px 0',
-                                         borderBottomWidth: '1px',
-                                         borderStyle: 'solid',
-                                         borderColor: 'lightGrey2',
-                                         marginBottom: '30px'
-                                     },
-                                     '.yourir-chart-price-fill': {
-                                         fill: `url(#priceGradient)`
-                                     },
-                                     '.yourir-chart-price': {
-                                         stroke: 'black',
-                                         strokeWidth: '1px',
-                                     },
-                                     '.yourir-chart-yaxis-label': {
-                                         position: 'relative',
-                                         top: '-10px'
-                                     },
-                                     '.yourir-chart-yaxis-left .yourir-chart-yaxis-outside .yourir-chart-yaxis-label-container': {
-                                         left: '0',
-                                         marginRight: '0',
-                                         fontSize: '12px',
-                                         color: 'grey'
-                                     },
-                                     '.yourir-chart-xaxis-label': {
-                                         fontSize: '12px',
-                                         color: 'grey'
-                                     },
-                                     '.yourir-chart-panel-border-bottom': {
-                                         display: 'none'
-                                     },
-                                     '.yourir-chart-tick-bottom': {
-                                         display: 'none'
-                                     },
-                                     '.yourir-chart-xaxis-outside': {
-                                         position: 'relative',
-                                         top: '10px'
-                                     },
-                                     '.yourir-chart-yaxis-gridline': {
-                                         stroke: 'lightGrey2'
-                                     }
-                                 }}>
+                                sx={{
+                                    '.yourir-chart': {
+                                        padding: '40px 0',
+                                        borderBottomWidth: '1px',
+                                        borderStyle: 'solid',
+                                        borderColor: 'lightGrey2',
+                                        marginBottom: '30px'
+                                    },
+                                    '.yourir-chart-price-fill': {
+                                        fill: `url(#priceGradient)`
+                                    },
+                                    '.yourir-chart-price': {
+                                        stroke: 'black',
+                                        strokeWidth: '1px',
+                                    },
+                                    '.yourir-chart-yaxis-label': {
+                                        position: 'relative',
+                                        top: '-10px'
+                                    },
+                                    '.yourir-chart-yaxis-left .yourir-chart-yaxis-outside .yourir-chart-yaxis-label-container': {
+                                        left: '0',
+                                        marginRight: '0',
+                                        fontSize: '12px',
+                                        color: 'grey'
+                                    },
+                                    '.yourir-chart-xaxis-label': {
+                                        fontSize: '12px',
+                                        color: 'grey'
+                                    },
+                                    '.yourir-chart-panel-border-bottom': {
+                                        display: 'none'
+                                    },
+                                    '.yourir-chart-tick-bottom': {
+                                        display: 'none'
+                                    },
+                                    '.yourir-chart-xaxis-outside': {
+                                        position: 'relative',
+                                        top: '10px'
+                                    },
+                                    '.yourir-chart-yaxis-gridline': {
+                                        stroke: 'lightGrey2'
+                                    }
+                                }}>
                                 <div id="priceComparisionChart" data-yourir="priceComparisonChart1 comparisonSymbol1=sol.asx volume.visible=false range=1d ranges=1d,1m,6m,1y,5y,10y showTooltips=true">
                                     <Flex>
                                         <Box>
@@ -183,8 +183,8 @@ const SharePricePanel = ({ _title }:any) : ReactNode => {
                                                 {({ isOpen }) => (
                                                     <>
                                                         <MenuButton as={Button}
-                                                                    variant="sharePrice"
-                                                                    rightIcon={isOpen ? <Icon icons={Icons.ChevronRight} /> : <Icon icons={Icons.ChevronDown} /> }>
+                                                            variant="sharePrice"
+                                                            rightIcon={isOpen ? <Icon icons={Icons.ChevronRight} /> : <Icon icons={Icons.ChevronDown} /> }>
                                                             {selectedSymbolLabel ? selectedSymbolLabel : 'Compare…'}
                                                         </MenuButton>
                                                         <Portal>
@@ -192,14 +192,14 @@ const SharePricePanel = ({ _title }:any) : ReactNode => {
                                                                 {
                                                                     comparisonSymbols.map((item, index) => {
                                                                         return <MenuItem key={index}
-                                                                                         as={Button}
-                                                                                         variant="menuItemFilter"
-                                                                                         onClick={() => {
-                                                                                             selectedSymbolLabel = item.name;
-                                                                                             set(`priceComparisionChart.comparisonSymbol2`, item.value);
-                                                                                         }}>
+                                                                            as={Button}
+                                                                            variant="menuItemFilter"
+                                                                            onClick={() => {
+                                                                                selectedSymbolLabel = item.name;
+                                                                                set(`priceComparisionChart.comparisonSymbol2`, item.value);
+                                                                            }}>
                                                                             {item.name}
-                                                                        </MenuItem>
+                                                                        </MenuItem>;
                                                                     })
                                                                 }
                                                             </MenuList>
@@ -295,44 +295,44 @@ const SharePricePanel = ({ _title }:any) : ReactNode => {
                         <Flex direction="row" justify="space-between" align="center" mt={8}>
                             <Box>
                                 <Button variant="sharePrice"
-                                        data-yourir="prevPage"
-                                        onClick={scrollToHistoricPriceTable}>
+                                    data-yourir="prevPage"
+                                    onClick={scrollToHistoricPriceTable}>
                                     Prev
                                 </Button>
                             </Box>
                             <Box>
                                 <ButtonGroup
                                     sx={{ '.yourir-active': {
-                                            bg: 'ghostWhite',
-                                            borderRadius: 0
-                                        }}}>
+                                        bg: 'ghostWhite',
+                                        borderRadius: 0
+                                    }}}>
                                     <Button variant="sharePrice"
-                                            data-yourir="navPage1"
-                                            onClick={scrollToHistoricPriceTable}>
+                                        data-yourir="navPage1"
+                                        onClick={scrollToHistoricPriceTable}>
                                     </Button>
                                     <Button variant="sharePrice"
-                                            data-yourir="navPage2"
-                                            onClick={scrollToHistoricPriceTable}>
+                                        data-yourir="navPage2"
+                                        onClick={scrollToHistoricPriceTable}>
                                     </Button>
                                     <Button variant="sharePrice"
-                                            data-yourir="navPage3"
-                                            onClick={scrollToHistoricPriceTable}>
+                                        data-yourir="navPage3"
+                                        onClick={scrollToHistoricPriceTable}>
                                     </Button>
                                     <Button variant="sharePrice"
-                                            data-yourir="navPage4"
-                                            onClick={scrollToHistoricPriceTable}>
+                                        data-yourir="navPage4"
+                                        onClick={scrollToHistoricPriceTable}>
                                     </Button>
                                     <Button variant="sharePrice"
-                                            data-yourir="navPage5"
-                                            onClick={scrollToHistoricPriceTable}>
+                                        data-yourir="navPage5"
+                                        onClick={scrollToHistoricPriceTable}>
                                     </Button>
 
                                 </ButtonGroup>
                             </Box>
                             <Box>
                                 <Button variant="sharePrice"
-                                        data-yourir="nextPage"
-                                        onClick={scrollToHistoricPriceTable}>
+                                    data-yourir="nextPage"
+                                    onClick={scrollToHistoricPriceTable}>
                                     Next
                                 </Button>
                             </Box>
