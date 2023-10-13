1c81191cdb59aae04408f67f4ce1f510ef9467bd
Carl Benedict Rain <carl.rain@rainfoundry.com>
2023-10-13 15:04:10 +1100

    master * Add grid

--- components/blocks/Video.tsx
+++ components/blocks/Video.tsx
@@ -1,8 +1,10 @@
 import ContentBlock from "~/components/blocks/Content";
-import { ReactNode, useState } from 'react';
-import { Text, AspectRatio } from '@chakra-ui/react';
+import { ReactNode, useState} from 'react';
+import { AspectRatio, Text } from '@chakra-ui/react';
 import dynamic from 'next/dynamic';
 import { IVideo } from '~/interfaces/util/video';
+import { Row, Column, ColumnValues }  from '~/components/elements/grid/grid';
+
 const ReactPlayer:any = dynamic(() => import("react-player"), { ssr: false });
 
 interface IVideoBlock {
@@ -17,18 +19,22 @@ const VideoBlock:any = ({ title, video, videoEmbed, ...props }:IVideoBlock) : Re
     const [currentVideo] = useState<IVideo>(video ? video : videoEmbed);
 
     return <ContentBlock {...props} mb={8}>
-        {
-            currentVideo && <AspectRatio ratio={[29 / 18, 21 / 10, ,21 / 9]}>
-                <ReactPlayer
-                    controls
-                    width="100%"
-                    height="100%"
-                    url={currentVideo.url} />
-            </AspectRatio>
-        }
-        {
-            title && <Text as="small">{title}</Text>
-        }
+        <Row>
+            <Column width={[ColumnValues.Full, ,ColumnValues.TenTwelfths]}>
+                {
+                    currentVideo && <AspectRatio ratio={[29 / 18, 21 / 10, ,21 / 9]}>
+                    <ReactPlayer
+                      controls
+                      width="100%"
+                      height="100%"
+                      url={currentVideo.url} />
+                  </AspectRatio>
+                }
+                {
+                    title && <Text as="small">{title}</Text>
+                }
+            </Column>
+        </Row>
     </ContentBlock>;
 };
 
