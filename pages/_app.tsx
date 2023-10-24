df5ba499bc3c90a34387f05436f5c59923db5677
Carl Benedict Rain <carl.rain@rainfoundry.com>
2023-10-24 17:04:30 +1100

    master * Bump

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) : Reac
     return <ChakraProvider theme={theme}>
         <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta name="version" content="0.5.7" />
+            <meta name="version" content="0.5.8" />
         </Head>
         <Fonts />
         <Component {...pageProps} />
