2f260a826a56612e9b2882fe2adf06559478c66d
Carl Benedict Rain <carl.rain@rainfoundry.com>
2023-10-25 21:06:26 +1100

    master * Update code

--- components/elements/charts/bar/modules/Bars.tsx
+++ components/elements/charts/bar/modules/Bars.tsx
@@ -13,19 +13,23 @@ interface IData {
 }
 
 export const Bars:any = ({ values, height, xScale, yScale }:IBars) : ReactNode =>  {
+    const borderRadius:number = 3;
     return <>
         {
             (Array.isArray(values) && values.length > 0) &&  <g className="bars">
                 {values.map(({ value, label }) => (
-                    <rect
-                        rx="3"
-                        className="bar"
-                        key={`bar-${label}`}
-                        x={xScale(label)}
-                        y={yScale(value)}
-                        width={xScale.bandwidth()}
-                        height={Math.max(height - yScale(value), 0)}
-                    />
+                    <g clipPath={`inset(0 0 ${borderRadius}px 0)`}>
+                        <rect
+                            rx={borderRadius}
+                            ry={borderRadius}
+                            className="bar"
+                            key={`bar-${label}`}
+                            x={xScale(label)}
+                            y={yScale(value)}
+                            width={xScale.bandwidth()}
+                            height={(Math.max(height - yScale(value)) + borderRadius)}
+                        />
+                    </g>
                 ))}
             </g>
         }
