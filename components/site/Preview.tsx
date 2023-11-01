88edb9f0e886f5f2e5b9896eeb5d3ae6dfcc7718
Carl Benedict Rain <carl.rain@rainfoundry.com>
2023-11-01 12:34:49 +1100

    staging * Update code

--- components/site/Preview.tsx
+++ components/site/Preview.tsx
@@ -1,31 +1,26 @@
 import { ReactNode } from 'react';
 import { useRouter } from 'next/router';
-import { Box, Container, Text, Button } from '@chakra-ui/react';
+import { Box, Container, Text } from '@chakra-ui/react';
+import { SectionLink } from '~/components/elements/sectionLink';
 
 const Preview:any = () : ReactNode => {
     const router = useRouter();
     let slug:string = router.asPath;
 
-    const exit:any = () => {
-        fetch(`/api/exit-preview?slug=${slug}`, {
-            method: 'POST',
-        }).then((response) => {
-            if(response) {
-                router.push(slug);
-            }
-        });
+    function trimSlash(str){
+        str = str.startsWith('/') ? str.substr(1) : str;
+        return str;
     }
 
     return (
-
         <Box width="100%" left={0} right={0} bottom={0} position="fixed" zIndex="300" background="white" height="50px" lineHeight="50px" fontSize="16px" borderTop="1px solid" borderColor="black">
             <Container>
                 <Text mb={0} mr={2} as="span">
                     You are now in Preview Mode.
                 </Text>
-                <Button onClick={() => exit()} zIndex="300">
+                <SectionLink href={`/api/exit-preview?redirect=${trimSlash(slug)}`} zIndex="300">
                     Click here to leave Preview Mode
-                </Button>
+                </SectionLink>
             </Container>
         </Box>
     );
