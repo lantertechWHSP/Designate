e5d3bf5838b7c3556b42cc655085c712571fa9a7
Carl Benedict Rain <carl.rain@rainfoundry.com>
2023-11-03 10:56:50 +1100

    master * Bypass preview

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -1,7 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(_: NextApiRequest, res: NextApiResponse) : void {
-    res.setHeader('WWW-authenticate', 'Basic realm="Secure Area"');
-    res.statusCode = 401;
-    res.end(`Auth Required.`);
+export default function handler(req: NextApiRequest, res: NextApiResponse) : void {
+    // Bypass Preview
+    if(!req.nextUrl.pathname.startsWith('/api/preview')) {
+        res.setHeader('WWW-authenticate', 'Basic realm="Secure Area"');
+        res.statusCode = 401;
+        res.end(`Auth Required.`);
+    }
 }
