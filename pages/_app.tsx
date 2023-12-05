9016fdb460043fc17ec37e1020ad0af15cfc8740
Carl Benedict Rain <carl.rain@rainfoundry.com>
2023-12-05 20:50:44 +1100

    master * Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import type { ReactElement, ReactNode, useEffect } from 'react';
+import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
@@ -9,7 +9,6 @@ import Router from 'next/router';
 import ReactGA from 'react-ga4';
 
 export const GA_TRACKING_ID:string = process.env.NEXT_PUBLIC_GA_ID;
-// import * as gtag from 'lib/gtag';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
     getLayout?: (page: ReactElement) => ReactNode
@@ -26,15 +25,17 @@ declare global {
     }
 }
 
-ReactGA.initialize([
-    {
-        trackingId: GA_TRACKING_ID,
-    },
-]);
+if(GA_TRACKING_ID) {
+    ReactGA.initialize([
+        {
+            trackingId: GA_TRACKING_ID,
+        },
+    ]);
 
-Router.events.on('routeChangeComplete', (url) => {
-    ReactGA.send({ hitType: "pageview", page: url });
-});
+    Router.events.on('routeChangeComplete', (url) => {
+        ReactGA.send({ hitType: "pageview", page: url });
+    });
+}
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) : ReactNode {
     return <ChakraProvider theme={theme}>
@@ -43,20 +44,24 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) : Reac
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta name="version" content="0.9.0" />
         </Head>
-        <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-                strategy="afterInteractive" />
-        <Script
-            dangerouslySetInnerHTML={{
-                __html: `
+        {
+            GA_TRACKING_ID && <>
+                <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                        strategy="afterInteractive" />
+                <Script
+                    dangerouslySetInnerHTML={{
+                        __html: `
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
                     gtag('config', '${GA_TRACKING_ID}', {
-                      page_path: window.location.pathname,
+                        page_path: window.location.pathname,
                     });
               `,
-            }}
-        />
+                    }}
+                />
+            </>
+        }
         <Component {...pageProps} />
     </ChakraProvider>;
 }
