5f51c6529854d6f44ff4464f1e74332c98dd8b7d
Carl Benedict Rain <carl.rain@rainfoundry.com>
2023-12-05 17:11:13 +1100

    master * Update!!!

--- lib/gtag.tsx
+++ lib/gtag.tsx
@@ -8,6 +8,8 @@ declare global {
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview:any = (url:string) : void => {
+    alert('!');
+    alert(url);
     GA_TRACKING_ID &&
     window.gtag('config', GA_TRACKING_ID, {
         page_path: url
