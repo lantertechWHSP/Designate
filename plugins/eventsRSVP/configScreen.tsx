1eb3a9a05f48933d06e96abec4f6083ac7ca985a
Carl Rain <carl.rain@rainfoundry.com>
2024-06-16 21:45:43 +1000

    feature/events-form * Tinker

--- plugins/eventsRSVP/configScreen.tsx
+++ plugins/eventsRSVP/configScreen.tsx
@@ -1,4 +1,4 @@
-import { Canvas, Form, FieldGroup, TextField, SelectField, Section } from 'datocms-react-ui';
+import { Canvas, Form, FieldGroup, Button } from 'datocms-react-ui';
 import { useEffect, useCallback, useState } from 'react';
 import 'datocms-react-ui/styles.css';
 import { doQuery, queries } from "~/dato/api";
@@ -16,15 +16,6 @@ type Parameters = {
 };
 
 const EventsRSVPConfigScreen = ({ ctx }: PropTypes) : any => {
-    const [formValues, setFormValues] = useState<Partial<Parameters>>(
-        ctx.parameters,
-    );
-
-    // const update = useCallback((field, value) => {
-    //     const newParameters = { ...formValues, [field]: value };
-    //     setFormValues(newParameters);
-    //     ctx.setParameters(newParameters);
-    // }, [formValues, setFormValues, ctx.setParameters]);
 
     const [eventRSVPItems, setEventRSVPItems] = useState([]);
 
@@ -39,73 +30,58 @@ const EventsRSVPConfigScreen = ({ ctx }: PropTypes) : any => {
     }, [ctx.formValues]);
 
 
+    const download:any = () : void => {
+
+    };
+
     return (
         <Canvas ctx={ctx}>
             <FieldGroup>
                 {
-                    eventRSVPItems.length > 0 && <div className="ItemsTable">
-                        <div className="ItemsTable__header-row">
-                            <div className="ItemsTable__header-cell">Name</div>
-                            <div className="ItemsTable__header-cell">Email</div>
-                            <div className="ItemsTable__header-cell">Is Shareholder</div>
-                            <div className="ItemsTable__header-cell">Attending</div>
+                    eventRSVPItems.length > 0 && <>
+                        <div className="ItemsTable">
+                            <div className="ItemsTable__header-row">
+                                <div className="ItemsTable__header-cell">Name</div>
+                                <div className="ItemsTable__header-cell">Email</div>
+                                <div className="ItemsTable__header-cell">Is Shareholder</div>
+                                <div className="ItemsTable__header-cell">Attending</div>
+                            </div>
+                            <div className="ItemsTable__content">
+                                {
+                                    eventRSVPItems.map((item, index: number) => {
+                                        return <div className="ItemsTable__row" key={index}>
+                                            <div className="ItemsTable__cell">
+                                                {item.name}
+                                            </div>
+                                            <div className="ItemsTable__cell">
+                                                {item.email}
+                                            </div>
+                                            <div className="ItemsTable__cell">
+                                                {
+                                                    item.isShareholder && <BooleanCell />
+                                                }
+                                            </div>
+                                            <div className="ItemsTable__cell">
+                                                {
+                                                    item.attending && <BooleanCell />
+                                                }
+                                            </div>
+                                        </div>;
+                                    })
+                                }
+                            </div>
                         </div>
-                        <div className="ItemsTable__content">
-                            {
-                                eventRSVPItems.map((item, index: number) => {
-                                    return <div className="ItemsTable__row" key={index}>
-                                        <div className="ItemsTable__cell">
-                                            {item.name}
-                                        </div>
-                                        <div className="ItemsTable__cell">
-                                            {item.email}
-                                        </div>
-                                        <div className="ItemsTable__cell">
-                                            {
-                                                item.isShareholder && <BooleanCell />
-                                            }
-                                        </div>
-                                        <div className="ItemsTable__cell">
-                                            {
-                                                item.attending && <BooleanCell />
-                                            }
-                                        </div>
-                                    </div>;
-                                })
-                            }
+                        <div style={{ marginTop: 'var(--spacing-l)' }}>
+                            <Button buttonType="primary" onClick={download}>
+                                Download Spreadsheet
+                            </Button>
                         </div>
-                    </div>
+                    </>
                 }
             </FieldGroup>
-            <Form>
-                <FieldGroup>
-                    <input type="hidden" id="rsvp" name="rsvp" value={formValues.rsvp}/>
-                    {/*<TextField*/}
-                    {/*    id="rsvp"*/}
-                    {/*    name="rsvp"*/}
-                    {/*    label="RSVP"*/}
-                    {/*    required*/}
-                    {/*    */}
-                    {/*    onChange={update.bind(null, 'rsvp')}*/}
-                    {/*/>*/}
-                    {/*<SelectField*/}
-                    {/*    name="rsvp"*/}
-                    {/*    id="rsvp"*/}
-                    {/*    label="RSVP"*/}
-                    {/*    value={formValues.rsvp}*/}
-                    {/*    // selectInputProps={{*/}
-                    {/*    //     isMulti: true,*/}
-                    {/*    //     options: [*/}
-                    {/*    //         { label: 'Option 1', value: 'option1' },*/}
-                    {/*    //         { label: 'Option 2', value: 'option2' },*/}
-                    {/*    //         { label: 'Option 3', value: 'option3' },*/}
-                    {/*    //     ],*/}
-                    {/*    // }}*/}
-                    {/*    onChange={update.bind(null, 'rsvp')}*/}
-                    {/*/>*/}
-
-                </FieldGroup>
-            </Form>
+            {/*<Form>*/}
+            {/*    <input type="text" id="rsvp" name="rsvp" value={ctx.formValues.rsvp}/>*/}
+            {/*</Form>*/}
         </Canvas>
     );
 };
