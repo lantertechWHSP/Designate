81e2ddc3772d7c446d9c57fa197f870eabb8f6cb
Carl Rain <carl.rain@rainfoundry.com>
2024-06-21 15:53:03 +1000

    staging * Update code

--- dato/queries/eventRSVP.tsx
+++ dato/queries/eventRSVP.tsx
@@ -1,6 +1,6 @@
 export const eventRSVP:string = `
     query eventRsvps ($in:[ItemId], $first: IntType, $skip: IntType) {
-        eventRSVPS: allEventRsvps(filter: {id: {in: $in}}, first: $first, skip: $skip, orderBy: _createdAt_DESC) {
+        eventRSVPS: allEventRsvps(filter: {id: {in: $in}}, first: $first, skip: $skip, orderBy: _createdAt_ASC) {
             name
             isShareholder
             email
