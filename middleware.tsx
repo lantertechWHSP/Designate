3dfbc906885f7a1654553db238b7b16a529fa699
Carl Rain <carl.rain@rainfoundry.com>
2024-08-20 23:04:36 +1000

    master * Tinker

--- middleware.tsx
+++ middleware.tsx
@@ -6,7 +6,7 @@ export const config:any = {
 
 export async function middleware(request: NextRequest) : Promise<NextResponse> {
     // Staging Environment
-    if(process.env.SITE_ENVIRONMENT === 'staging' || process.env.SITE_ENVIRONMENT === 'preproduction') {
+    if(process.env.SITE_ENVIRONMENT === 'staging' || process.env.SITE_ENVIRONMENT === 'preproduction' || process.env.SITE_ENVIRONMENT === 'production') {
         const basicAuth:any = request.headers.get('authorization');
         const url:any = request.nextUrl;
 
