e1b75cf8dc524678b4dbd3cd7649f3fcfc82f72d
Carl Rain <carl.rain@rainfoundry.com>
2024-08-20 16:40:10 +1000

    master * Update

--- pages/documents/[slug].tsx
+++ pages/documents/[slug].tsx
@@ -1,42 +1,13 @@
 import React from 'react';
 import type { NextPage } from 'next';
-import { GetStaticPathsResult, GetStaticPropsContext, GetStaticPropsResult } from 'next';
+import { GetStaticPropsContext, GetStaticPropsResult } from 'next';
 import { doQuery, queries } from '~/dato/api';
-import { IPost } from '~/interfaces/models/post';
-import { IDocument } from "~/interfaces/models/document";
+import { IDocument } from '~/interfaces/models/document';
 
 interface INextPageProps {
-    post?:IPost;
-    document?:IDocument;
 }
 
-export async function getStaticPaths() : Promise<GetStaticPathsResult<any>> {
-    const documents = [];
-
-    let hasAllDocuments = false;
-    let documentBatchIndex = 0;
-
-    while(!hasAllDocuments) {
-        const batchDocuments = await doQuery(queries.documents, { first: 100, skip: 100 * documentBatchIndex }).then(({ documents }) => documents);
-
-        documents.push(...batchDocuments);
-
-        if(batchDocuments.length < 100) {
-            hasAllDocuments = true;
-        }
-        else {
-            documentBatchIndex++;
-        }
-    }
-
-    const paths:any = (Array.isArray(documents) && documents.length > 0) ? documents.map((post) => ({
-        params: { slug: post.slug }
-    })) : [];
-
-    return { paths, fallback: false };
-}
-
-export async function getStaticProps({ params, preview }:GetStaticPropsContext) : Promise<GetStaticPropsResult<INextPageProps>> {
+export async function getServerSideProps({ params, preview }:GetStaticPropsContext) : Promise<GetStaticPropsResult<INextPageProps>> {
     const { slug } = params;
     const document:IDocument = await doQuery(queries.document, { slug }, preview).then(
         ({ document }) => document
@@ -55,7 +26,7 @@ export async function getStaticProps({ params, preview }:GetStaticPropsContext) 
     return {
         redirect: {
             permanent: false,
-            destination: '/',
+            destination: '/error',
         },
         props:{},
     };
