b1c96e338363ba5470e134b5119cddea27296e9f
Carl Rain <carl.rain@rainfoundry.com>
2024-08-21 15:10:21 +1000

    master * Update code

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -1,15 +1,17 @@
-import type { NextPage } from 'next';
-import type {GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
+import { GetStaticPropsContext, GetStaticPropsResult, NextPage } from 'next';
 import { doQuery, queries } from '~/dato/api';
 import { getLayoutData } from '~/lib/utils';
 import { ISite } from '~/interfaces/layout/site';
 import { IPage } from '~/interfaces/models/page';
 import { ILayout } from '~/interfaces/layout/layout';
+import { useEffect } from 'react';
+import { IDocument } from '~/interfaces/models/document';
 
 interface INextPageProps {
+    document:IDocument;
 }
 
-export async function getServerSideProps({ }:GetServerSidePropsContext) : Promise<GetServerSidePropsResult<INextPageProps>> {
+export async function getStaticProps({ }:GetStaticPropsContext) : Promise<GetStaticPropsResult<INextPageProps>> {
     const slug:string = 'privacy-policy';
     const site:ISite = await doQuery(queries.site);
     const page:IPage = await doQuery(queries.page, { slug }, false).then(
@@ -19,15 +21,22 @@ export async function getServerSideProps({ }:GetServerSidePropsContext) : Promis
     const layout:ILayout = getLayoutData(site, page, false);
 
     return {
-        redirect: {
-            permanent: false,
-            destination: layout.footer.privacyPolicyDocument.document.url,
+        props: {
+            document: layout.footer.privacyPolicyDocument
         },
-        props:{},
     };
 }
 
-const PrivacyPolicyPage : NextPage = ({ }:INextPageProps)  : JSX.Element => {
+const PrivacyPolicyPage : NextPage = ({ document }:any)  : JSX.Element => {
+    useEffect(() => {
+        if(document.document.url) {
+            window.location.href = document.document.url;
+        }
+        else {
+            window.location.href = '/error';
+        }
+    }, []);
+
     return (
         <></>
     );
