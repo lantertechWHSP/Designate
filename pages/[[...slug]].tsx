c3c45c5e094d372703732cbfb63c8f2dce6fab25
Carl Rain <carl.rain@rainfoundry.com>
2024-08-29 21:48:07 +1000

    master * Update

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -69,7 +69,7 @@ export async function getStaticProps({ params, preview }:GetStaticPropsContext) 
             layout,
             blocks
         },
-        revalidate: 30
+        // revalidate: 30
     };
 }
 
